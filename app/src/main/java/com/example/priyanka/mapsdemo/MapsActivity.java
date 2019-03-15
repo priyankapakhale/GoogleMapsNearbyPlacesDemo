e3f9608af8843978e15dce9e181e6c5704ecab1e
Shivam-Maindola <shivammaindola07@gmail.com>
2019-03-15 15:47:11 +0530

    corrected

--- app/src/main/java/com/example/priyanka/mapsdemo/MapsActivity.java
+++ app/src/main/java/com/example/priyanka/mapsdemo/MapsActivity.java
@@ -146,6 +146,7 @@ LocationListener{
         GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData();
         Log.i("test", "onClick: ");
         Log.i("test2", "onClick: one more ");
+        Log.i("test3", "onClick: one more ");
 
         switch(v.getId())
         {
